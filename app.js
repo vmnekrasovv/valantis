573e816961055b330a92beac5d42940b3e17e46b
test <test@gmail.com>
2024-03-24 18:21:08 +0300

    v1.5 - abort controller
    Сложнее запись, для меня.. а так, отрабатывает так же

--- app.js
+++ app.js
@@ -3,128 +3,111 @@
 //const host = 'http://api.valantis.store:40000/';
 const host = 'https://api.valantis.store:41000/';
 const password = 'Valantis';
-const stamp = new Date().toISOString().slice(0,10).replace(/-/g,"");
-const hash = CryptoJS.MD5(`${password}_${stamp }`);
+const stamp = new Date().toISOString().slice(0, 10).replace(/-/g, "");
+const hash = CryptoJS.MD5(`${password}_${stamp}`);
 
 
 let config = {
 	method: 'POST',
-    headers: {
-    	'Accept': 'application/json',
+	headers: {
+		'Accept': 'application/json',
 		'Content-Type': 'application/json',
-      	'X-Auth': hash
-    }
+		'X-Auth': hash
+	}
 }
 
 let page = 1;
-let reset_flag = false;
 let filter = false;
 
 let products_list = document.querySelector('.products-list');
 let pagination_list = document.querySelector('.pagination-list');
 
+function start (promise = Promise.resolve()) {
+	const controller = new AbortController();
+	return {
+		controller,
+		promise: promise.then(() => main(controller.signal).catch(reason => console.error(reason)))
+	}
 
-async function main(){
+}
+async function main (signal) {
 
 	page = 1;
 	products_list.innerHTML = "";
 	pagination_list.innerHTML = "";
 
-	let ids = await get_ids();
+	let ids = await get_ids(signal);
 
 	for (let i = 0; i < ids.length; i += 50) {
-		
-		let items = await get_items(ids.slice(i, i + 50));
 
-		/*if(items){
+		let items = await get_items(ids.slice(i, i + 50), signal);
+
+		if (items) {
 			render(items);
-		}*/
-
-		/*if(reset_flag) {
-			break;
-		} else { render(items) }*/
+		}
 	}
 }
 
-let promise = main();
+let state = start();
 
 
 // get_ids 
 
-async function get_ids(){
+async function get_ids (signal) {
 
-	if(filter === false){
-		config.body = JSON.stringify({'action':'get_ids'});		
-	} 
+	if (filter === false) {
+		config.body = JSON.stringify({ 'action': 'get_ids' });
+	}
 	else {
 
-		let request = {'action':'filter', "params": {}};
+		let request = { 'action': 'filter', "params": {} };
 		request['params'][filter['name']] = filter['value'];
 
 		config.body = JSON.stringify(request);
-		
+
 	}
 
-	try {
-		
-		let response = await fetch(host, config);
+	config.signal = signal;
 
-		while (!response.ok) {
+	let response = await fetch(host, config);
 
-			console.log(response.status + " : " + response.statusText);
-			console.log('Promise resolved but HTTP status failed - get_ids - ');
+	while (!response.ok) {
 
-			response = await fetch(host, config);
-		}
+		console.log(response.status + " : " + response.statusText);
+		console.log('Promise resolved but HTTP status failed - get_ids - ');
 
-		console.log('Promise resolved and HTTP status is successful - get_ids -');
-
-		let ids = await response.json();
-		return ids.result;
+		response = await fetch(host, config);
 	}
-	catch (error){
-		console.error('Promise rejected ' + error);
-	}
+
+	console.log('Promise resolved and HTTP status is successful - get_ids -');
+
+	let ids = await response.json();
+	return ids.result;
 }
 
 
 // get_items 
+async function get_items (ids, signal) {
+	config.signal = signal;
 
-let controller = new AbortController();
+	config.body = JSON.stringify({ 'action': 'get_items', 'params': { 'ids': ids } });
 
-async function get_items(ids){
+	let response = await fetch(host, config);
 
-	
-	config.signal = controller.signal;
+	while (!response.ok) {
 
-	config.body = JSON.stringify({'action': 'get_items', 'params': {'ids': ids}});
+		console.log(response.status + " : " + response.statusText);
+		console.log('Promise resolved but HTTP status failed - get_items');
 
-	try {
-
-		let response = await fetch(host, config);
-
-		while (!response.ok) {
-			
-			console.log(response.status + " : " + response.statusText);
-			console.log('Promise resolved but HTTP status failed - get_items');
-			
-			response = await fetch(host, config);
-		}
-
-		console.log('Promise resolved and HTTP status is successful - get_items -');
-
-		let items = await response.json();
-		items = clean_items(items.result);
-		
-		return items;
-	} 
-
-	catch (error) {
-		promise.then(() => {
-			promise = main();
-		});
-		console.log(error);
+		response = await fetch(host, config);
 	}
+
+	console.log('Promise resolved and HTTP status is successful - get_items -');
+
+	let items = await response.json();
+	items = clean_items(items.result);
+
+	return items;
 }
 
 // фильтер выдачи 
@@ -134,20 +117,20 @@ let submit_button = document.querySelector('.submit-button');
 let reset_button = document.querySelector('.reset-button');
 
 
-for(let i = 0; i < filter_input.length; i++){
+for (let i = 0; i < filter_input.length; i++) {
 
 	let elem = filter_input[i];
 
-	elem.addEventListener('change', function(){
+	elem.addEventListener('change', function () {
 
-		if(elem.value !== ''){
-			for(let j = 0; j < filter_input.length; j++){
-				if(j !== i){
+		if (elem.value !== '') {
+			for (let j = 0; j < filter_input.length; j++) {
+				if (j !== i) {
 					filter_input[j].setAttribute('disabled', 'disabled');
 				}
 			}
 		} else {
-			for(let j = 0; j < filter_input.length; j++){
+			for (let j = 0; j < filter_input.length; j++) {
 				filter_input[j].removeAttribute('disabled');
 			}
 		}
@@ -156,87 +139,76 @@ for(let i = 0; i < filter_input.length; i++){
 }
 
 
-submit_button.addEventListener('click', function(){
-	
-	for(let i = 0; i < filter_input.length; i++){
+submit_button.addEventListener('click', function () {
+
+	for (let i = 0; i < filter_input.length; i++) {
 
 		let elem = filter_input[i];
 
-		if(elem.value !== '') {
+		if (elem.value !== '') {
 
 			filter = new Array;
-			
+
 			let filter_name = elem.getAttribute('name');
 			filter['name'] = filter_name;
-			
-			if(filter_name == 'price'){
+
+			if (filter_name == 'price') {
 				filter['value'] = Number(elem.value);
 			} else {
 				filter['value'] = elem.value.trim();
-			}	
+			}
 
 			break;
-		} 
+		}
 	}
 
-	if(filter['name']){
-		for(let i = 0; i < filter_input.length; i++){
+	if (filter['name']) {
+		for (let i = 0; i < filter_input.length; i++) {
 			filter_input[i].setAttribute('disabled', 'disabled');
 		}
 	}
 
-	reset_flag = true;
-
-	promise.then(() => {
-		reset_flag = false;
-		promise = main();
-	});
+	state.controller.abort('change filter');
+	state = start(state.promise);
 });
 
-reset_button.addEventListener('click', function(){
+reset_button.addEventListener('click', function () {
 
 	products_list.innerHTML = "";
 	pagination_list.innerHTML = "";
 
-	filter_input.forEach(function(elem){
+	filter_input.forEach(function (elem) {
 		elem.value = '';
 		elem.removeAttribute('disabled');
 	});
 
 	filter = false;
-	/*reset_flag = true;
 
-	promise.then(() => {
-		reset_flag = false;
-		promise = main();
-	});*/
-
-	controller.abort();
-
-
+	state.controller.abort('reset');
+	state = start(state.promise);
 });
 
 
 // отсечение дублей айди в items.result
 
-function clean_items(items){
+function clean_items (items) {
 
 	let clean_items = [];
 
-	for(let i = 0; i < items.length; i++){
+	for (let i = 0; i < items.length; i++) {
 
 		let unique = 1;
 
-		for(let key in clean_items) {
+		for (let key in clean_items) {
 			key === items[i]['id'] ? unique = 0 : unique = 1;
 		}
-		
+
 		unique == 1 ? clean_items[items[i]['id']] = items[i] : false;
 	}
 
 	items = [];
 
-	for(let key in clean_items) {
+	for (let key in clean_items) {
 		items.push(clean_items[key]);
 	}
 
@@ -246,11 +218,11 @@ function clean_items(items){
 
 // рендер айтем бокса
 
-function render_itemBox(){
+function render_itemBox () {
 
 	let item_box = document.createElement('div');
 
-	if(page === 1){
+	if (page === 1) {
 		item_box.className = 'item-box item-box-' + page + ' active';
 	} else {
 		item_box.className = 'item-box item-box-' + page;
@@ -262,13 +234,13 @@ function render_itemBox(){
 
 // рендер элементов 
 
-function render_items(items_item){
+function render_items (items_item) {
 
 	let item = document.createElement('div'),
-	item_id = document.createElement('div'),
-	item_product = document.createElement('div'),
-	item_price = document.createElement('div'),
-	item_brand = document.createElement('div');
+		item_id = document.createElement('div'),
+		item_product = document.createElement('div'),
+		item_price = document.createElement('div'),
+		item_brand = document.createElement('div');
 
 
 	item.className = 'item';
@@ -287,29 +259,29 @@ function render_items(items_item){
 
 	item.append(item_id, item_product, item_price, item_brand);
 
-	document.querySelector('.item-box-'+page).append(item);
+	document.querySelector('.item-box-' + page).append(item);
 }
 
 
 // рендер пагинации
 
-function render_pagination(){
+function render_pagination () {
 
 	let pagination_page = document.createElement('span');
-		pagination_page.className = 'pagination-page';
-		pagination_page.setAttribute('data', 'item-box-'+page);
-		pagination_page.append(page);
-		document.querySelector('.pagination-list').append(pagination_page);
+	pagination_page.className = 'pagination-page';
+	pagination_page.setAttribute('data', 'item-box-' + page);
+	pagination_page.append(page);
+	document.querySelector('.pagination-list').append(pagination_page);
 }
 
 
 // рендер всего
 
-function render(items){
+function render (items) {
 
 	render_itemBox();
 
-	for(let i = 0; i < items.length; i++){
+	for (let i = 0; i < items.length; i++) {
 		render_items(items[i], page);
 	}
 
@@ -321,15 +293,15 @@ function render(items){
 
 // событие клика по элементам пагинации
 
-pagination_list.addEventListener('DOMNodeInserted', function(event) {
-	
-	if(event.target.classList.contains('pagination-page')){
+pagination_list.addEventListener('DOMNodeInserted', function (event) {
 
-		event.target.addEventListener('click', function(event){
-		
+	if (event.target.classList.contains('pagination-page')) {
+
+		event.target.addEventListener('click', function (event) {
+
 			let data_class = event.target.attributes.data.nodeValue;
 
-			document.querySelectorAll('.item-box').forEach(function(elem){
+			document.querySelectorAll('.item-box').forEach(function (elem) {
 				elem.classList.remove('active');
 			});
 
